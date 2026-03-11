a48fdf61ca58ed030631abc982406ea9d3def42c
zatopik <almaz.android.10@gmail.com>
2026-03-11 15:41:39 +0300

    test flake

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Go development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            go
+            gopls
+            gotools
+            golangci-lint
+	    goose
+          ];
+
+          shellHook = ''
+            echo "Go Flake Shell loaded!"
+            go version
+          '';
+        };
+      });
+}
+
